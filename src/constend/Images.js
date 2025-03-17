0fd0ee1c87fcdc79cfb45f69e64a9c29f0c41495
Ankush Thakur <138371782+AnkushYT248@users.noreply.github.com>
2025-03-17 11:18:29 +0530

    Update Images.js
    updated the images location

--- src/constend/Images.js
+++ src/constend/Images.js
@@ -1,6 +1,6 @@
-const beach_image = "../public/images/placeholders/beach.webp";
-const google = "../public/images/svg/google.svg";
-const tides_images = "../public/images/placeholders/tides.webp";
+const beach_image = "../../public/images/placeholders/beach.webp";
+const google = "../../public/images/svg/google.svg";
+const tides_images = "../../public/images/placeholders/tides.webp";
 
 export const Images = {
   beach_image,
